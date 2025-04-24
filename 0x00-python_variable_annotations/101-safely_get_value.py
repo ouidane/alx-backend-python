3b43a600666e6174d0058df93d323b2fc392b54e
ouidane <ouidanezakaria@gmail.com>
2025-04-24 23:48:11 +0100

    fix annotations

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 """Function with proper type annotations using TypeVar."""
 
-from typing import Mapping, Any, Union, TypeVar
+from typing import Any, Mapping, Union, TypeVar
+
 
 T = TypeVar('T')
+Res = Union[Any, T]
+Def = Union[T, None]
 
-def safely_get_value(dct: Mapping, key: Any, 
-                     default: Union[T, None] = None) -> Union[Any, T]:
-    """Returns a value in a dictionary."""
+
+def safely_get_value(dct: Mapping, key: Any, default: Def = None) -> Res:
+    """Returns the value in a dictionary for a given key."""
     if key in dct:
         return dct[key]
     else:
--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
 """Function zoom_array with proper type annotations."""
 
-from typing import List, Any
+from typing import List, Tuple
 
-def zoom_array(lst: List[Any], factor: int = 2) -> List[Any]:
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
     """Return list of items from lst argument duplicated factor times"""
-    zoomed_in: List[Any] = [
+    zoomed_in: List = [
         item for item in lst
-        for i in range(factor)
+        for i in range(int(factor))
     ]
     return zoomed_in
 
 
-array = [12, 72, 91]
+array = (12, 72, 91)
 
 zoom_2x = zoom_array(array)
 zoom_3x = zoom_array(array, 3)
