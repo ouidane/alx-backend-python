80fe0a2ed628046ac89e4dea1f0ff877716dace8
ouidane <ouidanezakaria@gmail.com>
2025-04-24 23:22:49 +0100

    fix variable annotations

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
-''' Module for task 4 '''
-
+''' Variable annotations for specific types and values. '''
 
 a: int = 1
 pi: float = 3.14
 i_understand_annotations: bool = True
-school: str = 'ALX'
+school: str = "ALX"
