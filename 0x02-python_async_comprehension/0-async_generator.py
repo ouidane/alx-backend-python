624aa54bc91748248ba539c4aae679120d75f64d
ouidane <ouidanezakaria@gmail.com>
2025-04-29 23:50:06 +0100

    task 0

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Async generator coroutine that yields 10 random floats between 0 and 10."""
+import asyncio
+import random
+
+async def async_generator():
+    """Yield a random float between 0 and 10
+    10 times with 1-second delay.
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
